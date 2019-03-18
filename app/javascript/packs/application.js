065e51376d596ef37d85195fb87b9b3a43b44521
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-03-18 19:45:50 +0000

    Add JS for algolia places and update flats new view form accordingly

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 initMapbox();
+initAutocomplete();
 
