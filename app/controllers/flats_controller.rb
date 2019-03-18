71e3c9980248bd2654d720acf19dab633d233fee
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-03-18 17:44:54 +0000

    Update flats controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,7 +4,13 @@ class FlatsController < ApplicationController
   # GET /flats
   # GET /flats.json
   def index
-    @flats = Flat.all
+    @flats = Flat.where.not(latitude: nil, longitude: nil)
+    @markers = @flats.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 
   # GET /flats/1
