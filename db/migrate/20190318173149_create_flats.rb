6638566b41c5a04a2da2113b50ecf930aaff90ba
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-03-18 17:33:40 +0000

    Create Flat model and add lat and lng

---
+++ db/migrate/20190318173149_create_flats.rb
@@ -0,0 +1,12 @@
+class CreateFlats < ActiveRecord::Migration[5.2]
+  def change
+    create_table :flats do |t|
+      t.string :name
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
