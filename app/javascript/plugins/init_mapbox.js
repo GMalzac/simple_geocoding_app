9bc3dd44356210bf00126f11ccb40767d0bc778d
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-03-18 19:55:28 +0000

    Add dark skin to the map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -9,7 +9,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/greg-malzac/cjterprzb14ey1fqpmebgntw5'
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
@@ -23,11 +23,9 @@ const initMapbox = () => {
     });
 
     const fitMapToMarkers = (map, markers) => {
-      console.log("entering fitMapToMarkers");
       const bounds = new mapboxgl.LngLatBounds();
       markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
       map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
-      console.log("exiting fitMapToMarkers");
     };
 
     fitMapToMarkers(map, markers);
